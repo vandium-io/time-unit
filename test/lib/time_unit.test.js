8c10e68360b3e3d1839c778cb2a8a2988a9de61a
Richard Hyatt <richardhyatt@me.com>
2016-06-03 10:50:37 -0400

    updated tests

--- test/lib/time_unit.test.js
+++ test/lib/time_unit.test.js
@@ -18,11 +18,11 @@ describe( 'lib/time_unit', function() {
 
                 expect( timeUnit.nanoseconds.toNanos( 1 ) ).to.equal( 1 );
                 expect( timeUnit.nanoseconds.toMicros( 1000 ) ).to.equal( 1 );
-                expect( timeUnit.nanoseconds.toMillis( 1000000 ) ).to.equal( 1 );
-                expect( timeUnit.nanoseconds.toSeconds( 1000000000 ) ).to.equal( 1 );
-                expect( timeUnit.nanoseconds.toMinutes( 60000000000 ) ).to.equal( 1 );
-                expect( timeUnit.nanoseconds.toHours( 3600000000000 ) ).to.equal( 1 );
-                expect( timeUnit.nanoseconds.toDays( 86400000000000  ) ).to.equal( 1 );
+                expect( timeUnit.nanoseconds.toMillis( 1000*1000 ) ).to.equal( 1 );
+                expect( timeUnit.nanoseconds.toSeconds( 1000*1000*1000 ) ).to.equal( 1 );
+                expect( timeUnit.nanoseconds.toMinutes( 1000*1000*1000*60 ) ).to.equal( 1 );
+                expect( timeUnit.nanoseconds.toHours( 1000*1000*1000*60*60 ) ).to.equal( 1 );
+                expect( timeUnit.nanoseconds.toDays( 1000*1000*1000*60*60*24 ) ).to.equal( 1 );
             });
         });
 
@@ -35,10 +35,10 @@ describe( 'lib/time_unit', function() {
                 expect( timeUnit.microseconds.toNanos( 1 ) ).to.equal( 1000 );
                 expect( timeUnit.microseconds.toMicros( 1 ) ).to.equal( 1 );
                 expect( timeUnit.microseconds.toMillis( 1000 ) ).to.equal( 1 );
-                expect( timeUnit.microseconds.toSeconds( 1000000 ) ).to.equal( 1 );
-                expect( timeUnit.microseconds.toMinutes( 60000000 ) ).to.equal( 1 );
-                expect( timeUnit.microseconds.toHours( 3600000000 ) ).to.equal( 1 );
-                expect( timeUnit.microseconds.toDays( 86400000000  ) ).to.equal( 1 );
+                expect( timeUnit.microseconds.toSeconds( 1000*1000 ) ).to.equal( 1 );
+                expect( timeUnit.microseconds.toMinutes( 1000*1000*60 ) ).to.equal( 1 );
+                expect( timeUnit.microseconds.toHours( 1000*1000*60*60 ) ).to.equal( 1 );
+                expect( timeUnit.microseconds.toDays( 1000*1000*60 *60*24 ) ).to.equal( 1 );
             });
         });
 
@@ -48,13 +48,13 @@ describe( 'lib/time_unit', function() {
 
                 expect( timeUnit.milliseconds ).to.equal( timeUnit.MILLISECONDS );
 
-                expect( timeUnit.milliseconds.toNanos( 1 ) ).to.equal( 1000000 );
+                expect( timeUnit.milliseconds.toNanos( 1 ) ).to.equal( 1000*1000 );
                 expect( timeUnit.milliseconds.toMicros( 1 ) ).to.equal( 1000 );
                 expect( timeUnit.milliseconds.toMillis( 1 ) ).to.equal( 1 );
                 expect( timeUnit.milliseconds.toSeconds( 1000 ) ).to.equal( 1 );
-                expect( timeUnit.milliseconds.toMinutes( 60000 ) ).to.equal( 1 );
-                expect( timeUnit.milliseconds.toHours( 3600000 ) ).to.equal( 1 );
-                expect( timeUnit.milliseconds.toDays( 86400000  ) ).to.equal( 1 );
+                expect( timeUnit.milliseconds.toMinutes( 1000*60 ) ).to.equal( 1 );
+                expect( timeUnit.milliseconds.toHours( 1000*60*60 ) ).to.equal( 1 );
+                expect( timeUnit.milliseconds.toDays( 1000*60*60*24 ) ).to.equal( 1 );
             });
         });
 
@@ -64,13 +64,13 @@ describe( 'lib/time_unit', function() {
 
                 expect( timeUnit.seconds ).to.equal( timeUnit.SECONDS );
 
-                expect( timeUnit.seconds.toNanos( 0.001 ) ).to.equal( 1000000 );    // prec will be off otherwise
-                expect( timeUnit.seconds.toMicros( 1 ) ).to.equal( 1000000 );
+                expect( timeUnit.seconds.toNanos( 1) ).to.equal( 1000*1000*1000 );
+                expect( timeUnit.seconds.toMicros( 1 ) ).to.equal( 1000*1000 );
                 expect( timeUnit.seconds.toMillis( 1 ) ).to.equal( 1000 );
                 expect( timeUnit.seconds.toSeconds( 1 ) ).to.equal( 1 );
                 expect( timeUnit.seconds.toMinutes( 60 ) ).to.equal( 1 );
-                expect( timeUnit.seconds.toHours( 3600 ) ).to.equal( 1 );
-                expect( timeUnit.seconds.toDays( 86400  ) ).to.equal( 1 );
+                expect( timeUnit.seconds.toHours( 60*60 ) ).to.equal( 1 );
+                expect( timeUnit.seconds.toDays( 60*60*24 ) ).to.equal( 1 );
             });
         });
 
@@ -80,9 +80,9 @@ describe( 'lib/time_unit', function() {
 
                 expect( timeUnit.minutes ).to.equal( timeUnit.MINUTES );
 
-                expect( timeUnit.minutes.toNanos( 0.001 ) ).to.equal( 60000000 );    // prec will be off otherwise
-                expect( timeUnit.minutes.toMicros( 1 ) ).to.equal( 60000000 );
-                expect( timeUnit.minutes.toMillis( 1 ) ).to.equal( 60000 );
+                expect( timeUnit.minutes.toNanos( 1 ) ).to.equal( 60*1000*1000*1000 );
+                expect( timeUnit.minutes.toMicros( 1 ) ).to.equal( 60*1000*1000 );
+                expect( timeUnit.minutes.toMillis( 1 ) ).to.equal( 60*1000 );
                 expect( timeUnit.minutes.toSeconds( 1 ) ).to.equal( 60 );
                 expect( timeUnit.minutes.toMinutes( 1 ) ).to.equal( 1 );
                 expect( timeUnit.minutes.toHours( 60 ) ).to.equal( 1 );
@@ -96,13 +96,13 @@ describe( 'lib/time_unit', function() {
 
                 expect( timeUnit.hours ).to.equal( timeUnit.HOURS );
 
-                expect( timeUnit.hours.toNanos( 0.001 ) ).to.equal( 3600000000 );    // prec will be off otherwise
-                expect( timeUnit.hours.toMicros( 0.01 ) ).to.equal( 36000000 );
-                expect( timeUnit.hours.toMillis( 0.1 ) ).to.equal( 360000 );
-                expect( timeUnit.hours.toSeconds( 1 ) ).to.equal( 3600 );
+                expect( timeUnit.hours.toNanos( 1 ) ).to.equal( 60*60*1000*1000*1000 );
+                expect( timeUnit.hours.toMicros( 1 ) ).to.equal( 60* 60*1000*1000 );
+                expect( timeUnit.hours.toMillis( 1 ) ).to.equal( 60* 60*1000 );
+                expect( timeUnit.hours.toSeconds( 1 ) ).to.equal( 60*60 );
                 expect( timeUnit.hours.toMinutes( 1 ) ).to.equal( 60 );
                 expect( timeUnit.hours.toHours( 1 ) ).to.equal( 1 );
-                expect( timeUnit.hours.toDays( 24  ) ).to.equal( 1 );
+                expect( timeUnit.hours.toDays( 24 ) ).to.equal( 1 );
             });
         });
 
